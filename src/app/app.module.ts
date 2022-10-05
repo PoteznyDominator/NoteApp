d7c3b6f0357a5514c2041626becd0b4a51f5f90f
PoteznyDominator <b.murawski00@gmail.com>
2022-10-05 11:55:34 +0200

    Adding user sidebar for app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent }         from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule }     from '@angular/material/sidenav';
+import { MatButtonModule }      from "@angular/material/button";
+import { MatListModule }        from "@angular/material/list";
+import { MatIconModule }        from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -10,9 +14,13 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers:    [],
+  bootstrap:    [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
